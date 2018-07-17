da5441d7460d4ac3f81b70e911bb2b8dcd757787
Samay Shamdasani <samay@Samays-MacBook-Pro.local>
2018-07-17 15:05:11 -0400

    should work now

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,5 +2,5 @@ module.exports = {
   siteMetadata: {
     title: "Saint Peter's Prep Science Department",
   },
-  plugins: ['gatsby-plugin-react-helmet'],
+  plugins: ['gatsby-plugin-react-helmet', 'gatsby-plugin-react-next'],
 }
